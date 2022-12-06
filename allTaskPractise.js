277fc6418e97b230cdf5d67070d38c7113e2fa80
Pavle Jovanovic <paycheta@gmail.com>
2022-12-06 11:26:31 +0100

    datatypes introduction

--- allTaskPractise.js
+++ allTaskPractise.js
@@ -5,7 +5,26 @@
 // document.writeln(X);
 // document.writeln(_x);
 
-let a = 5;
-let b = 8;
-let c = a + b;
-document.writeln(c);
+// let a = 5;
+// let b = 8;
+// let c = a + b;
+// document.writeln(c);
+
+let x = 15;
+let name = "John";
+let result = name + x;
+document.writeln(result);
+
+let x1 = 15;
+let y = 5;
+let name1 = "John";
+let result1 = x + y + name;
+let result2 = name + x + y;
+document.writeln(result1);
+document.writeln(result2);
+
+let x2 = 55;
+let x3 = x2 / "text";
+let x4 = x / 0;
+document.writeln(x3);
+document.writeln(x4);
