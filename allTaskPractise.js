0c0ca6e263b0e8138272ce96ae47f7e5ab47b0a8
Pavle Jovanovic <paycheta@gmail.com>
2022-12-06 11:13:32 +0100

    Variable practise

---
+++ allTaskPractise.js
@@ -0,0 +1,11 @@
+// var x = 55;
+// var X = "abc";
+// var _x = 1.2;
+// document.writeln(x);
+// document.writeln(X);
+// document.writeln(_x);
+
+let a = 5;
+let b = 8;
+let c = a + b;
+document.writeln(c);
